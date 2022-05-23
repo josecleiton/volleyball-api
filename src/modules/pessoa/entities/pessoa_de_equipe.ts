78b6c6d2822c6a463d24fd64eaa81dbaf33427b8
José Cleiton Correia <jcleitonbc@gmail.com>
2022-05-23 07:28:31 -0300

    fix(VT-66): adiciona indice na pessoa de equipe

--- src/modules/pessoa/entities/pessoa_de_equipe.ts
+++ src/modules/pessoa/entities/pessoa_de_equipe.ts
@@ -1,7 +1,8 @@
-import { Column } from 'typeorm';
+import { Column, Index } from 'typeorm';
 import { TemPessoa } from './tem_pessoa.entity';
 
 export abstract class PessoaDeEquipe extends TemPessoa {
   @Column('uuid')
+  @Index()
   idEquipe!: string;
 }
