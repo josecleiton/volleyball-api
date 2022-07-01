77e7c56932cf0cdc4e39e9ef8f6113165df4a63f
José Cleiton <jcleitonbc@gmail.com>
2022-06-30 22:05:03 -0300

    feat(VT-127): valida diferença de 2 pontos no overtime para vitória

--- test/modules/partida/fluxos/partida-concluida.e2e-spec.ts
+++ test/modules/partida/fluxos/partida-concluida.e2e-spec.ts
@@ -3,8 +3,6 @@ import { PontosPartida } from 'src/modules/partida/enums/pontos-partida.enum';
 import { initTestingApp } from 'test/modules/helpers';
 import { PartidaConcluidaServer } from './partida-concluida.server';
 
-process.env.LOGGING = '1';
-
 describe('Fluxo - PartidaResultado (e2e)', () => {
   let app: INestApplication;
   let server: PartidaConcluidaServer;
@@ -38,6 +36,25 @@ describe('Fluxo - PartidaResultado (e2e)', () => {
           PontosPartida.VITORIA_PERFEITA,
         );
         expect(partida.mandante?.pontuacao).toEqual(PontosPartida.DERROTA_FEIA);
+      });
+    });
+
+    describe('Unprocessable Entity', () => {
+      it('Diferença entre pontos no set não é 2 para sets com mais de 25 pontos', async () => {
+        const { partidaInicializada } =
+          await server.partida.criaPartidaInicializada();
+
+        await expect(
+          server.cadastraResultado(partidaInicializada.id, {
+            setsMandante: [25, 13, 18],
+            setsVisitante: [28, 25, 25],
+          }),
+        ).rejects.toThrow('422');
+
+        const partida = await server.partida.encontraPartida(
+          partidaInicializada.id,
+        );
+        expect(partida.ganhadora).toBeFalsy();
       });
     });
   });
