da1946eff0bf73a88d0c2939bcbe4c05d70da217
José Cleiton <jcleitonbc@gmail.com>
2022-06-30 23:19:18 -0300

    test(VT-127): adiciona casos de falha na verificação da requisição

--- test/modules/partida/fluxos/partida-concluida.e2e-spec.ts
+++ test/modules/partida/fluxos/partida-concluida.e2e-spec.ts
@@ -1,3 +1,4 @@
+import faker = require('faker');
 import { INestApplication } from '@nestjs/common';
 import { PontosPartida } from 'src/modules/partida/enums/pontos-partida.enum';
 import { initTestingApp } from 'test/modules/helpers';
@@ -37,9 +38,100 @@ describe('Fluxo - PartidaResultado (e2e)', () => {
         );
         expect(partida.mandante?.pontuacao).toEqual(PontosPartida.DERROTA_FEIA);
       });
+
+      it('Vitória perfeita (3-0)', async () => {
+        const { partidaInicializada } =
+          await server.partida.criaPartidaInicializada();
+
+        const partida = await server.cadastraResultado(partidaInicializada.id, {
+          setsVisitante: [12, 13, 18],
+          setsMandante: [25, 25, 25],
+        });
+
+        expect(partida.ganhadora).toBeTruthy();
+        expect(partida.idEquipeGanhador).toEqual(partida.idEquipeMandante);
+        expect(partida.ganhadora?.pontuacao).toEqual(
+          PontosPartida.VITORIA_PERFEITA,
+        );
+        expect(partida.visitante?.pontuacao).toEqual(
+          PontosPartida.DERROTA_FEIA,
+        );
+        expect(partida.mandante?.pontuacao).toEqual(
+          PontosPartida.VITORIA_PERFEITA,
+        );
+      });
+
+      it('Vitória simples (3-2)', async () => {
+        const { partidaInicializada } =
+          await server.partida.criaPartidaInicializada();
+
+        const partida = await server.cadastraResultado(partidaInicializada.id, {
+          setsMandante: [12, 13, 25, 25, 25],
+          setsVisitante: [25, 25, 18, 20, 22],
+        });
+
+        expect(partida.ganhadora).toBeTruthy();
+        expect(partida.idEquipeGanhador).toEqual(partida.idEquipeMandante);
+        expect(partida.ganhadora?.pontuacao).toEqual(
+          PontosPartida.VITORIA_SIMPLES,
+        );
+        expect(partida.visitante?.pontuacao).toEqual(
+          PontosPartida.DERROTA_SIMPLES,
+        );
+        expect(partida.mandante?.pontuacao).toEqual(
+          PontosPartida.VITORIA_SIMPLES,
+        );
+      });
+
+      it('Derrota simples (3-2)', async () => {
+        const { partidaInicializada } =
+          await server.partida.criaPartidaInicializada();
+
+        const partida = await server.cadastraResultado(partidaInicializada.id, {
+          setsMandante: [25, 25, 18, 20, 22],
+          setsVisitante: [12, 13, 25, 25, 25],
+        });
+
+        expect(partida.ganhadora).toBeTruthy();
+        expect(partida.idEquipeGanhador).toEqual(partida.idEquipeVisitante);
+        expect(partida.ganhadora?.pontuacao).toEqual(
+          PontosPartida.VITORIA_SIMPLES,
+        );
+        expect(partida.visitante?.pontuacao).toEqual(
+          PontosPartida.VITORIA_SIMPLES,
+        );
+        expect(partida.mandante?.pontuacao).toEqual(
+          PontosPartida.DERROTA_SIMPLES,
+        );
+      });
     });
 
     describe('Unprocessable Entity', () => {
+      it('Status não é participante cadastrado', async () => {
+        const { partidas } =
+          await server.partida.fluxoLigaIniciada.criaLigaInicializada();
+        const partida = faker.random.arrayElement(partidas);
+
+        await expect(
+          server.cadastraResultado(partida.id, {
+            setsMandante: [25, 25, 25],
+            setsVisitante: [0, 0, 0],
+          }),
+        ).rejects.toThrow('422');
+      });
+
+      it('Cardinalidade dos sets não são iguais', async () => {
+        const { partidaInicializada } =
+          await server.partida.criaPartidaInicializada();
+
+        await expect(
+          server.cadastraResultado(partidaInicializada.id, {
+            setsMandante: [25, 13, 18, 25],
+            setsVisitante: [28, 25, 25],
+          }),
+        ).rejects.toThrow('422');
+      });
+
       it('Diferença entre pontos no set não é 2 para sets com mais de 25 pontos', async () => {
         const { partidaInicializada } =
           await server.partida.criaPartidaInicializada();
@@ -48,6 +140,23 @@ describe('Fluxo - PartidaResultado (e2e)', () => {
           server.cadastraResultado(partidaInicializada.id, {
             setsMandante: [25, 13, 18],
             setsVisitante: [28, 25, 25],
+          }),
+        ).rejects.toThrow('422');
+
+        const partida = await server.partida.encontraPartida(
+          partidaInicializada.id,
+        );
+        expect(partida.ganhadora).toBeFalsy();
+      });
+
+      it('Uma equipe venceu mais do que o necessário em uma partida. Ex: 4-1', async () => {
+        const { partidaInicializada } =
+          await server.partida.criaPartidaInicializada();
+
+        await expect(
+          server.cadastraResultado(partidaInicializada.id, {
+            setsMandante: [25, 13, 18, 10],
+            setsVisitante: [28, 25, 25, 25],
           }),
         ).rejects.toThrow('422');
 
